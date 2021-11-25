bb10df8e979e1a99a5644c6e6444f2d4397a86a9
tomorow94 <40265192+tomorow94@users.noreply.github.com>
2021-11-25 18:36:53 +0100

    Added first generic class called check and some example tests

---
+++ GenericsExamples/Check.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModellbasierteSoftwareEntwicklung.GenericsExamples
+{
+	internal class Check<T>
+	{
+		public bool Compare(T a, T b)
+		{
+			if (a != null && a.Equals(b))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ModellbasierteSoftwareEntwicklung.GenericsExamples;
+
+Console.WriteLine("Hello, World!");
+
+//Check.cs examples
+Console.WriteLine("|---------------------------------|");
+Console.WriteLine("check integers:");
+var checkInt = new Check<int>();
+Console.WriteLine("1 & 3");
+Console.WriteLine(checkInt.Compare(1, 3));
+Console.WriteLine("2 & 2");
+Console.WriteLine(checkInt.Compare(2, 2));
+Console.WriteLine("|---------------------------------|");
+Console.WriteLine("check strings:");
+var checkString = new Check<string>();
+Console.WriteLine("hi & Hi");
+Console.WriteLine(checkString.Compare("hi", "Hi"));
+Console.WriteLine("Hi & Hi");
+Console.WriteLine(checkString.Compare("Hi", "Hi"));
+Console.WriteLine("hi & ho");
+Console.WriteLine(checkString.Compare("hi", "ho"));
+Console.WriteLine("|---------------------------------|");
+Console.WriteLine("check bool:");
+var checkBool = new Check<bool>();
+Console.WriteLine("true & false");
+Console.WriteLine(checkBool.Compare(true, false));
+Console.WriteLine("true & true");
+Console.WriteLine(checkBool.Compare(true, true));
+Console.WriteLine("false & false");
+Console.WriteLine(checkBool.Compare(false, false));
+Console.WriteLine("|---------------------------------|");
+Console.WriteLine("check actions<int>:");
+Check<Action<int>> check = new Check<Action<int>>();
+Action<int> printAction1 = i => Console.WriteLine(i);
+Action<int> printAction2 = i => Console.WriteLine(i);
+Console.WriteLine("Action<int> printAction1 = i => Console.WriteLine(i);");
+Console.WriteLine("Action<int> printAction2 = i => Console.WriteLine(i);");
+Console.WriteLine("printAction1 & printAction1");
+Console.WriteLine(check.Compare(printAction1, printAction1));
+Console.WriteLine("printAction1 & printAction2");
+Console.WriteLine(check.Compare(printAction1, printAction2));
+Console.WriteLine("printAction1 & i => Console.WriteLine(i)");
+Console.WriteLine(check.Compare(printAction1, i => Console.WriteLine(i)));
+Console.WriteLine("i => Console.WriteLine(i) & i => Console.WriteLine(i)");
+Console.WriteLine(check.Compare(i => Console.WriteLine(i), i => Console.WriteLine(i)));
+Console.WriteLine("|---------------------------------|");
