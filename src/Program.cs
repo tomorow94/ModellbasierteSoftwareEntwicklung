d52438174ccbb97723847816b3772fc40d8ffdbb
tomorow94 <40265192+tomorow94@users.noreply.github.com>
2021-11-26 14:46:00 +0100

    added little menu to not always print everything for readability purpose

--- src/Program.cs
+++ src/Program.cs
@@ -3,7 +3,24 @@
 using ModellbasierteSoftwareEntwicklung.GenericsExamples;
 using ModellbasierteSoftwareEntwicklung.src.GenericsExamples;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Modellbasierte Software Entwicklung : Generics in C#!");
 
-CheckExamples.Print();
+char key = 'x';
+
+while (key!='0')
+{
+	Console.WriteLine();
+	Console.WriteLine("1 : print 'Check' examples");
+	Console.WriteLine("0 : exit");
+	Console.WriteLine();
+	key = Console.ReadKey().KeyChar;
+	switch (key)
+	{
+		case '1':
+			CheckExamples.Print();
+			break;
+		default:
+			break;
+	}
+}
 
